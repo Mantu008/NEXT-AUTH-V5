304459c3cbd37aadbb6353be161821b251bfab8d
mantu <mantukumar87586299@gmail.com>
2024-08-31 12:31:52 +0530

    some ui chenge

--- auth/src/app/page.tsx
+++ auth/src/app/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const router = useRouter();
     const handledbconnect = async () => {
-        toast.success("ok");
+        toast.success("Redirected To Login page..");
+        router.push("/login");
     };
 
     return (
         <div className="flex flex-col justify-center items-center mt-[200px] gap-10">
             <h1>hello</h1>
             <Button variant="outline" onClick={handledbconnect}>
-                Button
+                Login
             </Button>
             <Toaster />
         </div>
