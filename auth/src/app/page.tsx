f4fdc604b1c41a5e869d42a67c05c47348091b1c
mantu <mantukumar87586299@gmail.com>
2024-08-31 10:17:45 +0530

    chenge

--- auth/src/app/page.tsx
+++ auth/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>hello</h1>
+            <h2>hhjuiuj</h2>
         </div>
     );
 }
