775f7e01830540dc1e7edcab5ce2ddddce9b9a36
mantu <mantukumar87586299@gmail.com>
2024-08-31 10:25:57 +0530

    somthing chenge

--- auth/src/app/page.tsx
+++ auth/src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>hello</h1>
-            <h2>hhjuiuj</h2>
-        </div>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24"></div>
     );
 }
